6bf94c2ee1761ee0b7a9a729e716ebfa1c7fe63b
Dmitry Savitski <dimasavitski@gmail.com>
2018-04-21 11:45:46 +0300

    Add AltorosToken

--- contracts/AltorosToken.sol
+++ contracts/AltorosToken.sol
@@ -1,5 +1,15 @@
 pragma solidity 0.4.23;
 
-contract AltorosToken {
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
+contract AltorosToken is MintableToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    function AltorosToken() public {
+        name = "Altoros Token";
+        symbol = "ALT";
+        decimals = 18;
+    }
 }
